f50cd18b82c929ad474205964a5486ddcff14ffa
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2026-05-29 10:09:05 +0100

    Adjust cosine similarity score to reduce number of results returned

--- Koha/SearchEngine/Elasticsearch/Search.pm
+++ Koha/SearchEngine/Elasticsearch/Search.pm
@@ -694,7 +694,7 @@ sub semantic_search {
     return ( "Could not generate query embedding", undef, [] ) unless $vector;
 
     my $body = {
-        min_score => 1.3,
+        min_score => 1.6,
         query     => {
             script_score => {
                 query  => { bool => { filter => { exists => { field => 'embedding' } } } },
